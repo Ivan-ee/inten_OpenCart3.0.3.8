ff521c645579eb4f03ca3aaf8206b8d802542bc7
Smirnov Ivan <smirnowwwivan@mail.ru>
2023-11-23 23:42:08 +0300

    update menu icons

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -1,46 +1,56 @@
 <?php
 class ControllerCommonMenu extends Controller {
-	public function index() {
-		$this->load->language('common/menu');
+    public function index() {
+        $this->load->language('common/menu');
 
-		// Menu
-		$this->load->model('catalog/category');
+        // Menu
+        $this->load->model('catalog/category');
 
-		$this->load->model('catalog/product');
+        $this->load->model('catalog/product');
 
-		$data['categories'] = array();
+        $this->load->model('tool/image');
 
-		$categories = $this->model_catalog_category->getCategories(0);
+        $data['categories'] = array();
 
-		foreach ($categories as $category) {
-			if ($category['top']) {
-				// Level 2
-				$children_data = array();
+        $categories = $this->model_catalog_category->getCategories(0);
 
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
+        foreach ($categories as $category) {
 
-				foreach ($children as $child) {
-					$filter_data = array(
-						'filter_category_id'  => $child['category_id'],
-						'filter_sub_category' => true
-					);
+            if($category['image']){
+                $image = $this->model_tool_image->resize($category['image'], 40, 40);
+            } else {
+                $image = false;
+            }
 
-					$children_data[] = array(
-						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
-					);
-				}
+            if ($category['top']) {
+                // Level 2
+                $children_data = array();
 
-				// Level 1
-				$data['categories'][] = array(
-					'name'     => $category['name'],
-					'children' => $children_data,
-					'column'   => $category['column'] ? $category['column'] : 1,
-					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
-				);
-			}
-		}
+                $children = $this->model_catalog_category->getCategories($category['category_id']);
 
-		return $this->load->view('common/menu', $data);
-	}
+                foreach ($children as $child) {
+                    $filter_data = array(
+                        'filter_category_id'  => $child['category_id'],
+                        'filter_sub_category' => true
+                    );
+
+                    $children_data[] = array(
+                        'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                        'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+                    );
+                }
+
+                // Level 1
+                $data['categories'][] = array(
+                    'name'     => $category['name'],
+                    'image'    => $image,
+                    'children' => $children_data,
+                    'column'   => $category['column'] ? $category['column'] : 1,
+                    'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+                );
+            }
+        }
+
+        return $this->load->view('common/menu', $data);
+    }
 }
