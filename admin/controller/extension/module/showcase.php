17fc992f20c75e74f45efd64b1d3d1c7523d8f68
Smirnov Ivan <smirnowwwivan@mail.ru>
2024-01-11 22:07:20 +0300

    update controller

--- admin/controller/extension/module/showcase.php
+++ admin/controller/extension/module/showcase.php
@@ -9,47 +9,47 @@ class ControllerExtensionModuleShowcase extends Controller {
 
         $this->load->model('extension/module/showcase');
 
-        $this->getList();
-    }
-
-    public function add() {
-        $this->load->language('design/banner');
-
-        $this->document->setTitle($this->language->get('heading_title'));
-
-        $this->load->model('design/banner');
-
-        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
-            $this->model_extension_module_showcase->addBanner($this->request->post);
-
-            $this->session->data['success'] = $this->language->get('text_success');
-
-            $url = '';
-
-            if (isset($this->request->get['sort'])) {
-                $url .= '&sort=' . $this->request->get['sort'];
-            }
-
-            if (isset($this->request->get['order'])) {
-                $url .= '&order=' . $this->request->get['order'];
-            }
-
-            if (isset($this->request->get['page'])) {
-                $url .= '&page=' . $this->request->get['page'];
-            }
-
-            $this->response->redirect($this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . $url, true));
-        }
-
         $this->getForm();
     }
 
+//    public function add() {
+//        $this->load->language('design/banner');
+//
+//        $this->document->setTitle($this->language->get('heading_title'));
+//
+//        $this->load->model('design/banner');
+//
+//        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
+//            $this->model_extension_module_showcase->addBanner($this->request->post);
+//
+//            $this->session->data['success'] = $this->language->get('text_success');
+//
+//            $url = '';
+//
+//            if (isset($this->request->get['sort'])) {
+//                $url .= '&sort=' . $this->request->get['sort'];
+//            }
+//
+//            if (isset($this->request->get['order'])) {
+//                $url .= '&order=' . $this->request->get['order'];
+//            }
+//
+//            if (isset($this->request->get['page'])) {
+//                $url .= '&page=' . $this->request->get['page'];
+//            }
+//
+//            $this->response->redirect($this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . $url, true));
+//        }
+//
+//        $this->getForm();
+//    }
+
     public function edit() {
-        $this->load->language('design/banner');
+        $this->load->language('extension/module/showcase');
 
         $this->document->setTitle($this->language->get('heading_title'));
 
-        $this->load->model('design/banner');
+        $this->load->model('extension/module/showcase');
 
         if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
             $this->model_extension_module_showcase->editBanner($this->request->get['banner_id'], $this->request->post);
@@ -70,194 +70,19 @@ class ControllerExtensionModuleShowcase extends Controller {
                 $url .= '&page=' . $this->request->get['page'];
             }
 
-            $this->response->redirect($this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . $url, true));
+            $this->response->redirect($this->url->link('extension/module/showcase', 'user_token=' . $this->session->data['user_token'] . $url, true));
         }
 
         $this->getForm();
     }
 
-    public function delete() {
-        $this->load->language('design/banner');
-
-        $this->document->setTitle($this->language->get('heading_title'));
-
-        $this->load->model('design/banner');
-
-        if (isset($this->request->post['selected']) && $this->validateDelete()) {
-            foreach ($this->request->post['selected'] as $banner_id) {
-                $this->model_extension_module_showcase->deleteBanner($banner_id);
-            }
-
-            $this->session->data['success'] = $this->language->get('text_success');
-
-            $url = '';
-
-            if (isset($this->request->get['sort'])) {
-                $url .= '&sort=' . $this->request->get['sort'];
-            }
-
-            if (isset($this->request->get['order'])) {
-                $url .= '&order=' . $this->request->get['order'];
-            }
-
-            if (isset($this->request->get['page'])) {
-                $url .= '&page=' . $this->request->get['page'];
-            }
-
-            $this->response->redirect($this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . $url, true));
-        }
-
-        $this->getList();
-    }
-
-    protected function getList() {
-        if (isset($this->request->get['sort'])) {
-            $sort = $this->request->get['sort'];
-        } else {
-            $sort = 'name';
-        }
-
-        if (isset($this->request->get['order'])) {
-            $order = $this->request->get['order'];
-        } else {
-            $order = 'ASC';
-        }
-
-        if (isset($this->request->get['page'])) {
-            $page = (int)$this->request->get['page'];
-        } else {
-            $page = 1;
-        }
-
-        $url = '';
-
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
-
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
-
-        if (isset($this->request->get['page'])) {
-            $url .= '&page=' . $this->request->get['page'];
-        }
-
-        $data['breadcrumbs'] = array();
-
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_home'),
-            'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
-        );
-
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . $url, true)
-        );
-
-        $data['add'] = $this->url->link('design/banner/add', 'user_token=' . $this->session->data['user_token'] . $url, true);
-        $data['delete'] = $this->url->link('design/banner/delete', 'user_token=' . $this->session->data['user_token'] . $url, true);
-
-        $data['banners'] = array();
-
-        $filter_data = array(
-            'sort'  => $sort,
-            'order' => $order,
-            'start' => ($page - 1) * $this->config->get('config_limit_admin'),
-            'limit' => $this->config->get('config_limit_admin')
-        );
-
-        $banner_total = $this->model_extension_module_showcase->getTotalBanners();
-
-        $results = $this->model_extension_module_showcase->getBanners($filter_data);
-
-        foreach ($results as $result) {
-            $data['banners'][] = array(
-                'banner_id' => $result['banner_id'],
-                'name'      => $result['name'],
-                'status'    => ($result['status'] ? $this->language->get('text_enabled') : $this->language->get('text_disabled')),
-                'edit'      => $this->url->link('design/banner/edit', 'user_token=' . $this->session->data['user_token'] . '&banner_id=' . $result['banner_id'] . $url, true)
-            );
-        }
-
-        if (isset($this->error['warning'])) {
-            $data['error_warning'] = $this->error['warning'];
-        } else {
-            $data['error_warning'] = '';
-        }
-
-        if (isset($this->session->data['success'])) {
-            $data['success'] = $this->session->data['success'];
-
-            unset($this->session->data['success']);
-        } else {
-            $data['success'] = '';
-        }
-
-        if (isset($this->request->post['selected'])) {
-            $data['selected'] = (array)$this->request->post['selected'];
-        } else {
-            $data['selected'] = array();
-        }
-
-        $url = '';
-
-        if ($order == 'ASC') {
-            $url .= '&order=DESC';
-        } else {
-            $url .= '&order=ASC';
-        }
-
-        if (isset($this->request->get['page'])) {
-            $url .= '&page=' . $this->request->get['page'];
-        }
-
-        $data['sort_name'] = $this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . '&sort=name' . $url, true);
-        $data['sort_status'] = $this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . '&sort=status' . $url, true);
-
-        $url = '';
-
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
-
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
-
-        $pagination = new Pagination();
-        $pagination->total = $banner_total;
-        $pagination->page = $page;
-        $pagination->limit = $this->config->get('config_limit_admin');
-        $pagination->url = $this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . $url . '&page={page}', true);
-
-        $data['pagination'] = $pagination->render();
-
-        $data['results'] = sprintf($this->language->get('text_pagination'), ($banner_total) ? (($page - 1) * $this->config->get('config_limit_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_limit_admin')) > ($banner_total - $this->config->get('config_limit_admin'))) ? $banner_total : ((($page - 1) * $this->config->get('config_limit_admin')) + $this->config->get('config_limit_admin')), $banner_total, ceil($banner_total / $this->config->get('config_limit_admin')));
-
-        $data['sort'] = $sort;
-        $data['order'] = $order;
-
-        $data['header'] = $this->load->controller('common/header');
-        $data['column_left'] = $this->load->controller('common/column_left');
-        $data['footer'] = $this->load->controller('common/footer');
-
-        $this->response->setOutput($this->load->view('design/banner_list', $data));
-    }
-
     protected function getForm() {
-        $data['text_form'] = !isset($this->request->get['banner_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
+//        $data['text_form'] = !isset($this->request->get['banner_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
         if (isset($this->error['warning'])) {
             $data['error_warning'] = $this->error['warning'];
         } else {
             $data['error_warning'] = '';
-        }
-
-        if (isset($this->error['name'])) {
-            $data['error_name'] = $this->error['name'];
-        } else {
-            $data['error_name'] = '';
         }
 
         if (isset($this->error['banner_image'])) {
@@ -268,18 +93,6 @@ class ControllerExtensionModuleShowcase extends Controller {
 
         $url = '';
 
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
-
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
-
-        if (isset($this->request->get['page'])) {
-            $url .= '&page=' . $this->request->get['page'];
-        }
-
         $data['breadcrumbs'] = array();
 
         $data['breadcrumbs'][] = array(
@@ -289,30 +102,16 @@ class ControllerExtensionModuleShowcase extends Controller {
 
         $data['breadcrumbs'][] = array(
             'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . $url, true)
+            'href' => $this->url->link('extension/module/showcase', 'user_token=' . $this->session->data['user_token'] . $url, true)
         );
 
-        if (!isset($this->request->get['banner_id'])) {
-            $data['action'] = $this->url->link('design/banner/add', 'user_token=' . $this->session->data['user_token'] . $url, true);
-        } else {
-            $data['action'] = $this->url->link('design/banner/edit', 'user_token=' . $this->session->data['user_token'] . '&banner_id=' . $this->request->get['banner_id'] . $url, true);
-        }
+        $data['action'] = $this->url->link('extension/module/showcase/edit', 'user_token=' . $this->session->data['user_token'] . $url, true);
 
-        $data['cancel'] = $this->url->link('design/banner', 'user_token=' . $this->session->data['user_token'] . $url, true);
+        $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . $url, true);
 
-        if (isset($this->request->get['banner_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
-            $banner_info = $this->model_extension_module_showcase->getBanner($this->request->get['banner_id']);
-        }
+        $banner_info = $this->model_extension_module_showcase->getBanner($this->request->get['banner_id']);
 
         $data['user_token'] = $this->session->data['user_token'];
-
-        if (isset($this->request->post['name'])) {
-            $data['name'] = $this->request->post['name'];
-        } elseif (!empty($banner_info)) {
-            $data['name'] = $banner_info['name'];
-        } else {
-            $data['name'] = '';
-        }
 
         if (isset($this->request->post['status'])) {
             $data['status'] = $this->request->post['status'];
@@ -353,7 +152,6 @@ class ControllerExtensionModuleShowcase extends Controller {
                     'link'       => $banner_image['link'],
                     'image'      => $image,
                     'thumb'      => $this->model_tool_image->resize($thumb, 100, 100),
-                    'sort_order' => $banner_image['sort_order']
                 );
             }
         }
@@ -364,17 +162,17 @@ class ControllerExtensionModuleShowcase extends Controller {
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['footer'] = $this->load->controller('common/footer');
 
-        $this->response->setOutput($this->load->view('design/banner_form', $data));
+        $this->response->setOutput($this->load->view('extension/module/showcase', $data));
     }
 
     protected function validateForm() {
-        if (!$this->user->hasPermission('modify', 'design/banner')) {
+        if (!$this->user->hasPermission('modify', 'extension/module/showcase')) {
             $this->error['warning'] = $this->language->get('error_permission');
         }
 
-        if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
-            $this->error['name'] = $this->language->get('error_name');
-        }
+//        if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
+//            $this->error['name'] = $this->language->get('error_name');
+//        }
 
         if (isset($this->request->post['banner_image'])) {
             foreach ($this->request->post['banner_image'] as $language_id => $value) {
