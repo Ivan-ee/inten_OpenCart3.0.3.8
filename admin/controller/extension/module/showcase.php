e07b1be99899fbfb8d3ed07222bbb4324284268e
Smirnov Ivan <smirnowwwivan@mail.ru>
2024-01-13 23:40:57 +0300

    update admin MVC module showcase

--- admin/controller/extension/module/showcase.php
+++ admin/controller/extension/module/showcase.php
@@ -8,60 +8,14 @@ class ControllerExtensionModuleShowcase extends Controller
     {
         $this->load->language('extension/module/showcase');
 
-        $this->document->setTitle($this->language->get('heading_title'));
-
-        $this->load->model('extension/module/showcase');
-
-        $this->getForm();
-    }
-
-    public function edit()
-    {
-        $this->load->language('extension/module/showcase');
+        $this->load->model('localisation/language');
 
         $this->document->setTitle($this->language->get('heading_title'));
 
         $this->load->model('extension/module/showcase');
 
-        // Добавим проверку на наличие разрешения на модификацию
-        if (!$this->user->hasPermission('modify', 'extension/module/showcase')) {
-            $this->error['warning'] = $this->language->get('error_permission');
-        }
+        $this->load->model('tool/image');
 
-        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
-            $status = $this->request->post['status'];
-            $bannerImages = $this->request->post['banner_image'];
-
-
-
-            foreach ($bannerImages as $key => $data) {
-                $showcaseImagesId = $key;
-                $title = $data['title'];
-                $link = $data['link'];
-                $image = $data['image'];
-
-                //  catalog/showcase/div.actual-offers__section-img_container.png
-
-                $image = strstr($image, "catalog/");
-
-//                $this->tte($image);
-
-                $this->model_extension_module_showcase->updateShowcaseImage($showcaseImagesId, $title, $link, $image);
-
-
-                $this->session->data['success'] = $this->language->get('text_success');
-            }
-
-            $url = '';
-
-            $this->response->redirect($this->url->link('extension/module/showcase', 'user_token=' . $this->session->data['user_token'] . $url, true));
-        }
-
-        $this->getForm();
-    }
-
-    protected function getForm()
-    {
         if (isset($this->error['warning'])) {
             $data['error_warning'] = $this->error['warning'];
         } else {
@@ -82,47 +36,34 @@ class ControllerExtensionModuleShowcase extends Controller
             'href' => $this->url->link('extension/module/showcase', 'user_token=' . $this->session->data['user_token'] . $url, true)
         );
 
+        $data['user_token'] = $this->session->data['user_token'];
+
         $data['action'] = $this->url->link('extension/module/showcase/edit', 'user_token=' . $this->session->data['user_token'] . $url, true);
 
         $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . $url, true);
 
-        $data['user_token'] = $this->session->data['user_token'];
-
-
-
-//        if (isset($this->request->post['status'])) {
-//            $data['status'] = $this->request->post['status'];
-//        } elseif (!empty($banner_info)) {
-//            $data['status'] = $banner_info['status'];
-//        } else {
-//            $data['status'] = true;
-//        }
-
-        $this->load->model('localisation/language');
+        $data['status'] = $this->model_extension_module_showcase->getStatus();
 
         $data['languages'] = $this->model_localisation_language->getLanguages();
 
 
-        $banner_info = $this->model_extension_module_showcase->getBanner();
-
+        $banner_info = $this->model_extension_module_showcase->getBanners();
 
         $data['banners'] = array();
 
-        $this->load->model('tool/image');
+        $baseUrl = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+        $baseUrl .= $_SERVER['HTTP_HOST'];
+        $baseUrl .= str_replace('/admin', '', dirname($_SERVER['SCRIPT_NAME']));
 
         foreach ($banner_info as $banner) {
             $data['banners'][] = array(
                 'showcase_images_id' => $banner['showcase_images_id'],
-                'image' => $this->model_tool_image->resize($banner['image'], 100, 100),
+                'image' => $baseUrl . '/image/' . $banner['image'],
                 'title' => $banner['title'],
                 'link' => $banner['link'],
             );
 
-            $this->tt( $data['banners']);
         }
-
-
-
 
         $data['placeholder'] = $this->model_tool_image->resize('no_image.png', 100, 100);
 
@@ -131,6 +72,49 @@ class ControllerExtensionModuleShowcase extends Controller
         $data['footer'] = $this->load->controller('common/footer');
 
         $this->response->setOutput($this->load->view('extension/module/showcase', $data));
+
+    }
+
+    public function edit()
+    {
+        $this->load->language('extension/module/showcase');
+        $this->document->setTitle($this->language->get('heading_title'));
+        $this->load->model('extension/module/showcase');
+        $this->load->model('tool/image');
+
+        if (!$this->user->hasPermission('modify', 'extension/module/showcase')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST')) {
+            $status = $this->request->post['status'];
+            $this->model_extension_module_showcase->saveStatus($status);
+
+            $bannerImages = $this->request->post['banner_image'];
+
+            foreach ($bannerImages as $key => $data) {
+                $showcaseImagesId = $key;
+                $title = $data['title'];
+                $link = $data['link'];
+                $image = $data['image'];
+
+                $path_parts = explode('catalog', $image);
+                if (isset($path_parts[1])) {
+                    $image = 'catalog' . $path_parts[1];
+                } else {
+                    $image = 'catalog/' . basename($image);
+                }
+
+                $this->model_extension_module_showcase->updateShowcaseImage($showcaseImagesId, $title, $link, $image);
+
+                $this->session->data['success'] = $this->language->get('text_success');
+            }
+
+            $url = '';
+            $this->response->redirect($this->url->link('extension/module/showcase', 'user_token=' . $this->session->data['user_token'] . $url, true));
+        }
+
+        $this->index();
     }
 
     protected function validateForm()
@@ -142,21 +126,8 @@ class ControllerExtensionModuleShowcase extends Controller
         return !$this->error;
     }
 
-    protected function validateDelete()
+    function tte($str)
     {
-        if (!$this->user->hasPermission('modify', 'extension/module/showcase')) {
-            $this->error['warning'] = $this->language->get('error_permission');
-        }
-
-        return !$this->error;
-    }
-
-    function tt($str){
-        echo "<pre>";
-        print_r($str);
-        echo "</pre>";
-    }
-    function tte($str){
         echo "<pre>";
         print_r($str);
         echo "</pre>";
--- admin/model/extension/module/showcase.php
+++ admin/model/extension/module/showcase.php
@@ -8,10 +8,24 @@ class ModelExtensionModuleShowcase extends Model
     }
 
 
-    public function getBanner()
+    public function getBanners()
     {
         $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "showcase_images");
 
         return $query->rows;
+    }
+
+    public function saveStatus($status) {
+        $this->db->query("UPDATE " . DB_PREFIX . "showcase SET status = '" . (int)$status . "' WHERE id = '1'");
+    }
+
+    public function getStatus() {
+        $query = $this->db->query("SELECT status FROM " . DB_PREFIX . "showcase WHERE id = '1'");
+
+        if ($query->num_rows) {
+            return $query->row['status'];
+        } else {
+            return 0;
+        }
     }
 }
