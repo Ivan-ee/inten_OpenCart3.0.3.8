b9c78f3b2bc385731742fec0325d594063621cb1
Smirnov Ivan <smirnowwwivan@mail.ru>
2024-01-30 21:22:33 +0300

    update controller actions

--- catalog/controller/information/actions.php
+++ catalog/controller/information/actions.php
@@ -13,17 +13,59 @@ class ControllerInformationActions extends Controller {
 
         $newArray = [];
 
+        // Массив перевода месяцев
+        $monthTranslations = [
+            'January'   => 'января',
+            'February'  => 'февраля',
+            'March'     => 'марта',
+            'April'     => 'апреля',
+            'May'       => 'мая',
+            'June'      => 'июня',
+            'July'      => 'июля',
+            'August'    => 'августа',
+            'September' => 'сентября',
+            'October'   => 'октября',
+            'November'  => 'ноября',
+            'December'  => 'декабря',
+        ];
+
         foreach ($actions as $action) {
-            $settingArray = json_decode($action['setting'], true);
+            $action_info = json_decode($action['setting'], true);
 
-            $action['action_info'] = $settingArray;
+            $start_datetime = new DateTime($action_info['start_date']);
+            $end_datetime = new DateTime($action_info['end_date']);
 
-            $newArray[] = $action;
+            $start_formatted = $start_datetime->format('d ');
+
+            if ($start_datetime->format('Y') !== $end_datetime->format('Y')) {
+                $start_formatted .= $start_datetime->format(' Y');
+            } elseif ($start_datetime->format('n') !== $end_datetime->format('n')) {
+                $start_formatted .= $monthTranslations[$start_datetime->format('F')];
+            }
+
+            $end_formatted = $end_datetime->format('d ');
+
+            $end_formatted .= $monthTranslations[$end_datetime->format('F')];
+            $end_formatted .= $end_datetime->format(' Y');
+
+            $year_diff = $start_datetime->format('Y') !== $end_datetime->format('Y');
+            $month_diff = $start_datetime->format('n') !== $end_datetime->format('n');
+            $day_diff = $start_datetime->format('j') !== $end_datetime->format('j');
+
+            if ($year_diff || $month_diff || $day_diff) {
+                $date_range = "с {$start_formatted} по {$end_formatted}";
+            } else {
+                $date_range = "в {$start_formatted}";
+            }
+
+            $action_info['formatted_date_range'] = $date_range;
+
+            $newArray[] = $action_info;
         }
 
         $data['actions'] = $newArray;
 
-//        tt($data['actions']);
+        tt($data['actions']);
 
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['column_right'] = $this->load->controller('common/column_right');
